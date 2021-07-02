ebf656f1be595db95a3e91b09cac2a36dfb8eb62
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-07-02 15:23:41 +0300

    add files

---
+++ src/js/components/mySwiper.js
@@ -0,0 +1,11 @@
+const slider = document.querySelector('.swiper-container')
+
+let swiper = new Swiper('.swiper-container1', {
+  // Optional parameters
+   slidesPerView: 1,
+
+  // If we need pagination
+  spaceBetween: 10,
+
+    
+});
